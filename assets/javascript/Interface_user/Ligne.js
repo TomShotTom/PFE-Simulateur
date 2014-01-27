e5046d06327742c315b26339826098f9956a4e42
TomShotTom <t.lerminet@gmail.com>
2014-01-27 15:37:02 +0100

    Update Ligne.js

--- assets/javascript/Interface_user/Ligne.js
+++ assets/javascript/Interface_user/Ligne.js
@@ -1,84 +1,80 @@
 <script>
-
 $(function () {
-    $('#container_2').highcharts({
-
-        title: {
-            text: 'Ligne Numeros 1'
-        },
-
-subtitle: {
-    text: ''
-    },
-
-xAxis: {
-    categories: [
-    <?php
-
-    for($a=0;$a<count($kml);$a++)
-    {
-        if($a!=count($kml))
-        {
-        if( array_key_exists ($a,$kml))   echo '"'.$kml[$a]['arret'].'", ';
-
-        }
-else
-                                    {
-                                        if( array_key_exists ($a,$kml))  echo "'".$kml[$a]['arret']."'";
-                                        }
-
-}
-?>]
-},
-
-yAxis: {
-    title: {
-    text: ''
-    },
-
-plotLines: [{
-    value: 1,
-    width: 1,
-    color: '#808080'
-    }]
-},
-
-tooltip: {
-    valueSuffix: ''
-    },
-
-legend: {
-    layout: 'vertical',
-    align: 'center',
-    enabled: false,
-    verticalAlign: 'middle',
-    borderWidth: 1
-    },
-
-series: [{
-    name: 'Ligne 1',
-    data: [
-    <?php
-
-    for($a=0;$a<count($kml);$a++)
-    {
-
-        if($a!=count($kml))
-        {
-        if( array_key_exists ($a,$kml))  echo '1 , ';
-
-        }
-else
-                                       {
-                                           if( array_key_exists ($a,$kml))  echo '1  ';
-                                           }
-
-}
-?>
-]
-}
-]
+    $(document).ready(function() {
+        Highcharts.setOptions({
+            global: {
+                useUTC: false
+            }
+        });
+    
+        var chart;
+        $('#container').highcharts({
+            chart: {
+                type: 'spline',
+                animation: Highcharts.svg, // don't animate in old IE
+                marginRight: 10,
+                events: {
+                    load: function() {
+    
+                        // set up the updating of the chart each second
+                        var series = this.series[0];
+                        setInterval(function() {
+                            var x = (new Date()).getTime(), // current time
+                                y = Math.random();
+                            series.addPoint([x, y], true, true);
+                        }, 1000);
+                    }
+                }
+            },
+            title: {
+                text: 'Live random data'
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 150
+            },
+            yAxis: {
+                title: {
+                    text: 'Value'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                formatter: function() {
+                        return '<b>'+ this.series.name +'</b><br/>'+
+                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
+                        Highcharts.numberFormat(this.y, 2);
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            exporting: {
+                enabled: false
+            },
+            series: [{
+                name: 'Random data',
+                data: (function() {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+    
+                    for (i = -19; i <= 0; i++) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: Math.random()
+                        });
+                    }
+                    return data;
+                })()
+            }]
+        });
+    });
+    
 });
-});
-
 </script>
